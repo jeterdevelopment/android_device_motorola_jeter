0b683e098e655c3dcab2006821274235e2aa3514
Filthy Luis <notcarbide@users.noreply.github.com>
2018-10-12 20:18:37 -0500

    update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,10 +4,9 @@ TARGET_BOARD_VENDOR := motorola
 
 # Architecture
 TARGET_ARCH := arm
-# TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv7-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
 
 TARGET_BOARD_PLATFORM := msm8937
